56ad5b054fb430ee40ee84d1690e931b43f18898
Dominique MICHEL <dominique.michel@epita.fr>
2023-02-09 22:00:54 +0100

    uvw: add uvw base code

--- src/entrypoint.cc
+++ src/entrypoint.cc
@@ -1,11 +1,51 @@
 #include "entrypoint.h"
 
+#include <cassert>
+
 #include <amqpcpp.h>
+#include <uvw.hpp>
+#include <uvw/emitter.h>
+#include <uvw/handle.hpp>
+#include <uvw/stream.h>
+#include <uvw/tcp.h>
 #include "rbmq/uvConnectionHandler.h"
 
 namespace rbmq {
 
+void bindTcpClient(const std::shared_ptr<uvw::TCPHandle>& handle) {
+    using namespace uvw;
+    handle->on<ErrorEvent>([](const ErrorEvent &e, TCPHandle &handle){
+        std::cerr << "uvw::tcp error : `" << e.what() << "` !\n";
+        handle.close();
+    });
+    handle->on<ConnectEvent>([](const ConnectEvent &, TCPHandle &){
+        std::cout << "uvw::tcp connected !\n";
+    });
+    handle->on<CloseEvent>([](const CloseEvent &, TCPHandle &){
+        std::cout << "uvw::tcp closed !\n";
+    });
+
+}
+
 int entrypoint() {
+    std::shared_ptr<uvw::Loop> loop = uvw::Loop::getDefault();
+    assert(loop);
+    std::shared_ptr<uvw::TCPHandle> client = loop->resource<uvw::TCPHandle>();
+    assert(client);
+
+    bindTcpClient(client);
+
+    client->connect("127.0.0.1", 5672);
+
+    loop->run();
+
+    std::cout << "Finishing...\n";
+
+
+    return 0;
+}
+
+int ignoreMe() {
     // create an instance of your own connection handler
     UvConnectionHandler myHandler;
 
